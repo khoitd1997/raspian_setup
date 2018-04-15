07a57efcfaa802b55f10e8d61a0071132585ca90
khoitd1997 <khoidinhtrinh@gmail.com>
2018-04-14 17:29:52 -0700

    Split installation into embedded and GUI version

--- create_user.sh
+++ create_user.sh
@@ -1,27 +1,27 @@
-#!/bin/bash 
+#!/bin/bash
 # DON'T KEEP THIS FILE IN THE PI HOME DIR
-RED='\33[38;5;0196m' 
-CYAN='\033[38;5;087m' #for marking the being of a new sections 
-YELLOW='\033[38;5;226m' #for error 
-GREEN='\033[38;5;154m' #for general messages 
-RESET='\033[0m' #for resetting the color 
+RED='\33[38;5;0196m'
+CYAN='\033[38;5;087m' #for marking the being of a new sections
+YELLOW='\033[38;5;226m' #for error
+GREEN='\033[38;5;154m' #for general messages
+RESET='\033[0m' #for resetting the color
+
+set -e
 
 # change keyboard layout to make sure the rest of installation is correct
-sudo dpkg-reconfigure keyboard-configuration 
-
-
-echo XKBLAYOUT="us" | sudo tee /etc/default/keyboard
+sudo sed -i '/XKBLAYOUT/d' /etc/default/keyboard
+echo XKBLAYOUT=\"us\" | sudo tee /etc/default/keyboard
 
 
 #Handle creating new users
-GR="$(groups pi | sed 's/pi : //; s/pi //; s/ /,/g')" 
+GR="$(groups pi | sed 's/pi : //; s/pi //; s/ /,/g')"
 printf "${GREEN}Please input user name, no space: ${RESET}"
 read USER_NAME
-sudo adduser ${USER_NAME} 
-sudo usermod -a -G ${GR} ${USER_NAME} 
+sudo adduser ${USER_NAME}
+sudo usermod -a -G ${GR} ${USER_NAME}
 
 
-printf "${GREEN}Please enter the password of the new user ${RESET}"
+printf "${GREEN}Please enter the password of the new user\n ${RESET}"
 su - ${USER_NAME}
 cp /home/pi/.profile /home/pi/.bashrc ${HOME}
 
--- setup.sh
+++ setup.sh
@@ -1,79 +1,83 @@
-#!/bin/bash 
-#SETUP SCRIPTS FOR RASPIAN
-#chmod u+x setup.sh will make the file executable for only owner
+#!/bin/bash
 
 #list of general utilities without GUI
-LITE=0 #0 for barebone raspbian, 1 for raspbian with desktop
+IS_EMBEDDED=0 #0 for barebone raspbian, 1 for raspbian with desktop
 
-SOFTWARE_GENERAL_REPO_NON_GUI=" doxygen xclip checkinstall lm-sensors cmake valgrind gcc clang llvm emacs build-essential htop net-tools gnome-keyring  dos2unix ufw "
+# Needed by both embedded and non embedded
+SOFTWARE_UNIVERSAL=" checkinstall lm-sensors cmake gcc clang llvm build-essential htop net-tools gnome-keyring dos2unix ufw python3 python2"
 
-#list of software with GUI                        
-SOFTWARE_WITH_GUI=" gksu terminator guake ddd evince synaptic psensor gufw "
+# List of software for barebone embedded system
+SOFTWARE_EMBEDDED_LINUX=" "
 
-#list of dropped app
-SOFTWARE_DROPPED=" gitg"
+#list of software with GUI
+SOFTWARE_WITH_GUI=" valgrind doxygen xclip gksu terminator guake ddd evince synaptic psensor gufw emacs "
 
 #all tool chains and utilities
 ARM_TOOLCHAIN="gdb-arm-none-eabi openocd qemu gcc-arm-none-eabi"
 AVR_ARDUINO_TOOLCHAIN="arduino avrdude avr-libc simulavr"
-FULL="$ARM_TOOLCHAIN $AVR_ARDUINO_TOOLCHAIN" 
+FULL="$ARM_TOOLCHAIN $AVR_ARDUINO_TOOLCHAIN"
 
 #software not in current Ubuntu 16.04 repos
 SOFTWARE_GENERAL_NONREPO="\nFoxit_Reader Visual_Studio_Code\nSophos Veeam\n Chrome Segger-JLink\n"
 
 #Color Variables for output, chosen for best visibility
-#Consult the Xterm 256 color charts for more code 
+#Consult the Xterm 256 color charts for more code
 #format is \33 then <fg_bg_code>;5;<color code>m, 38 for foreground, 48 for background
-RED='\33[38;5;0196m' 
-CYAN='\033[38;5;087m' #for marking the being of a new sections 
-YELLOW='\033[38;5;226m' #for error 
-GREEN='\033[38;5;154m' #for general messages 
-RESET='\033[0m' #for resetting the color 
-set -e 
+RED='\33[38;5;0196m'
+CYAN='\033[38;5;087m' #for marking the being of a new sections
+YELLOW='\033[38;5;226m' #for error
+GREEN='\033[38;5;154m' #for general messages
+RESET='\033[0m' #for resetting the color
+set -e
 #-------------------------------------------------------------------------------
 
-mkdir ${HOME}/Workspace #Workspace for Visual Stuio Code 
-if [ $LITE -eq 1 ] ; then
-    SOFTWARE_GENERAL_REPO="${SOFTWARE_GENERAL_REPO_NON_GUI}${SOFTWARE_WITH_GUI}"
-else 
-    SOFTWARE_GENERAL_REPO="${SOFTWARE_GENERAL_REPO_NON_GUI}"
+# must not be pi or root to run this script
+if [ ${USER} = pi ] || [ ${USER} = root ]; then
+  echo "${GREEN}\nPlease create another user first and then run this script${RESET}\n"
+  exit 1
 fi
 
-if sudo apt-get update\
-&& sudo apt-get dist-upgrade\
-&& sudo apt-get install ${SOFTWARE_GENERAL_REPO}
-then
-printf "\n ${YELLOW}Basic Setup Done\n ${RESET}"
-else 
-printf "\n ${YELLOW}Failed in Basic update and install\n ${RESET}"
-exit 1 
-fi 
+printf  "${GREEN}\nInstalling Universal Software\n${RESET}"
 
-if sudo ufw enable >> ; then 
+sudo apt-get update >> /dev/null
+sudo apt-get dist-upgrade -y >> /dev/null
+sudo apt-get install ${SOFTWARE_UNIVERSAL} -y >> /dev/null
+
+if [ $IS_EMBEDDED -eq 1 ] ; then
+    printf  "${GREEN}\nInstalling Software For GUI version\n${RESET}"
+    sudo apt-get install ${SOFTWARE_WITH_GUI} -y >> /dev/null
+    mkdir ${HOME}/Workspace #Workspace for Visual Stuio Code
+else
+    printf  "${GREEN}\nInstalling Software For Embedded version\n${RESET}"
+    sudo apt-get install ${SOFTWARE_EMBEDDED_LINUX} -y >> /dev/null
+fi
+
+if sudo ufw enable ; then
 printf  "${GREEN}Firewall Enabled\n ${RESET}"
-else 
+else
 printf "\n ${YELLOW}Firewall failed to enable\n ${RESET}"
-exit 1 
+exit 1
 fi
 
+if [ $IS_EMBEDDED -eq 1 ] ; then
 printf "\n ${CYAN}--------DEV-TOOLS----------- ${RESET}"
-printf "${CYAN}\n Basic Install is done, please select additional install options: \n ${RESET}"
-printf  "${CYAN}1/Full 2/ARM 3/AVR 4/Exit${RESET}" 
+printf "${GREEN}\n Basic Install is done, please select additional install options: \n ${RESET}"
+printf  "${GREEN}1/Full 2/ARM 3/AVR 4/Exit${RESET}"
 read option
 
 case $option in #handle options
-    1) printf "${GREEN}\n installing $FULL\n ${RESET}" 
+    1) printf "${GREEN}\n installing $FULL\n ${RESET}"
     if ! sudo apt-get install $FULL; then
-    printf "${YELLOW}\n Failed to install full package\n ${RESET}" 
+    printf "${YELLOW}\n Failed to install full package\n ${RESET}"
     exit 1
     fi;;
     2) printf "${GREEN}\n installing $ARM_TOOLCHAIN\n ${RESET}"
-    if ! sudo apt-get install $ARM_TOOLCHAIN; then 
-    printf "${YELLOW}\n Failed to install ARM toolchain\n ${RESET}" 
+    if ! sudo apt-get install $ARM_TOOLCHAIN; then
+    printf "${YELLOW}\n Failed to install ARM toolchain\n ${RESET}"
     exit 1
     fi ;;
     3) printf "\n ${GREEN}installing $AVR_ARDUINO_TOOLCHAIN\n ${RESET}"
-    if ! sudo apt-get install $AVR_ARDUINO_TOOLCHAIN; then 
+    if ! sudo apt-get install $AVR_ARDUINO_TOOLCHAIN; then
     printf "\n ${YELLOW}Failed to install AVR toolchain\n ${RESET}"
     exit 1
     fi ;;
@@ -81,8 +85,13 @@ case $option in #handle options
     *) printf  "${YELLOW}\nInvalid options\n ${RESET}"
         exit 1;;
 esac
+fi
 
 printf "\n ${CYAN} --------POST-INST-----------\n ${RESET}"
-printf  " ${GREEN} Script successfully executed \nPlease install these additional software if needed ${RESET} ${SOFTWARE_GENERAL_NONREPO} ${RESET}" 
-exit 0
+if [ $IS_EMBEDDED -eq 1 ] ; then
+printf  " ${GREEN} Script successfully executed \nPlease install these additional software if needed ${RESET} ${SOFTWARE_GENERAL_NONREPO} ${RESET}"
+fi
 
+printf  "${GREEN}\nRebooting in 5 seconds${RESET}"
+sleep 5
+reboot
